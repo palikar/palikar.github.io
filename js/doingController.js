8ac3590721a681cf21fa586885330105ee1badc5
Stanislav Arnaudov <stanislav_ts@abv.bg>
2018-01-17 22:31:34 +0100

    Acc impl.wewqewqewq

--- js/doingController.js
+++ js/doingController.js
@@ -170,7 +170,7 @@ function doingService() {
                 var sum = Math.abs(x) + Math.abs(y) + Math.abs(z);
                 that.accArr.push(sum);
 
-                if (that.accArr.length < 50) {
+                if (that.accArr.length < 10) {
                     return;
                 }
                 var allSum = 0;
